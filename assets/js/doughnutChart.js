a71c675f5908edf52ef36ce852033663fc8dbfaa
edosugita <edosugita.es@gmail.com>
2024-08-24 20:24:46 +0700

    Fixing responsive on breakeven analysis

--- assets/js/doughnutChart.js
+++ assets/js/doughnutChart.js
@@ -102,7 +102,7 @@ $(document).ready(function () {
         },
       },
       responsive: true,
-      maintainAspectRatio: false,
+      maintainAspectRatio: true,
       cutout: cutoutPercentage,
     };
   }
